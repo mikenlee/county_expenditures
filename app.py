7abbab9d38184662aa54d57fc6f9691ea2d04fab
mikenlee <mikeleexp@gmail.com>
2021-02-06 13:58:12 -0500

    added heroku check

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# Heroku check
+is_heroku = False
+if 'IS_HEROKU' in os.environ:
+    is_heroku = True
+
 import pandas as pd
 import json
 from flask import Flask, jsonify, render_template
@@ -8,11 +13,21 @@ from sqlalchemy import create_engine
 # PyMySQL 
 import pymysql
 pymysql.install_as_MySQLdb()
+# Import your config file(s) and variable(s)
+if is_heroku == True:
+    # if IS_HEROKU is found in the environment variables, then use the rest
+    # NOTE: you still need to set up the IS_HEROKU environment variable on Heroku (it is not there by default)
+    remote_db_endpoint = os.environ.get('remote_db_endpoint')
+    remote_db_port = os.environ.get('remote_db_port')
+    remote_db_name = os.environ.get('remote_db_name')
+    remote_db_user = os.environ.get('remote_db_user')
+    remote_db_pwd = os.environ.get('remote_db_pwd')
 
-# Config variables
-import config
-from config import remote_db_endpoint, remote_db_port
-from config import remote_db_name, remote_db_user, remote_db_pwd
+else:
+    # Config variables
+    import config
+    from config import remote_db_endpoint, remote_db_port
+    from config import remote_db_name, remote_db_user, remote_db_pwd
 
 # Import Pandas
 import pandas as pd
