633e945708935d77299616d45394fddf856d04e4
mikenlee <mikeleexp@gmail.com>
2021-02-06 21:02:15 -0500

    added county info hover

--- static/js/app.js
+++ static/js/app.js
@@ -21,7 +21,7 @@ myMap.dragging.disable();
 
 // Grabbing our GeoJSON data.
 d3.json("/api/geojson/ncr").then(data => {
- 
+  console.log(data);
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
     // Style each feature (in this case a county)
@@ -36,31 +36,41 @@ d3.json("/api/geojson/ncr").then(data => {
 
     // Called on each feature
     onEachFeature: function(feature, layer) {
-
+      var originalText = d3.select('#county-info').html()
 
       // Set mouse events to change map styling
       layer.on({
         // When a user's mouse touches a map feature (county), the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
         mouseover: function(event) {
-          
           layer = event.target;
           layer.setStyle({
             fillOpacity: .8            
           });
+
+          d3.select('#county-info').html(
+            `<br>
+            <h5><strong>${feature.properties.county}</strong></h5>
+            <strong>Population: ${feature.properties.population}</strong>
+            <strong></strong>`
+            )
                 
         },
         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
         mouseout: function(event) {
-          
           layer = event.target;
           layer.setStyle({
             fillOpacity: 0.01
           });
+
+          d3.select('#county-info').html(
+            originalText
+            )
+
         },
         // When a feature (county) is clicked, it displays information in html
-        click: function(event) {
-          myMap.fitBounds(event.target.getBounds());
-        }
+        // click: function(event) {
+        //   myMap.fitBounds(event.target.getBounds());
+        // }
       });
       
 
